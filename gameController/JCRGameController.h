d276f95e073eb912ee7fb568b9cad777231a717f
César Pinto Castillo <cesar@jagcesar.se>
2014-04-20 00:24:11 -0700

    Created JCRGameController The idea is for this object to be a representation of each connected game controller.

---
+++ gameController/JCRGameController.h
@@ -0,0 +1,13 @@
+//
+//  JCRGameController.h
+//  gameController
+//
+//  Created by César Manuel Pinto Castillo on 20/04/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCRGameController : NSObject
+
+@end
---
+++ gameController/JCRGameController.m
@@ -0,0 +1,13 @@
+//
+//  JCRGameController.m
+//  gameController
+//
+//  Created by César Manuel Pinto Castillo on 20/04/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import "JCRGameController.h"
+
+@implementation JCRGameController
+
+@end
