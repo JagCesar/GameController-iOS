b8395045a52b22a2a9a592566966fa3e1262d3a6
César Pinto Castillo <cesar@jagcesar.se>
2014-05-02 11:38:07 -0700

    Create JCRGameControllerManager

---
+++ gameController/JCRGameControllerManager.h
@@ -0,0 +1,13 @@
+//
+//  JCRGameControllerManager.h
+//  gameController
+//
+//  Created by César Manuel Pinto Castillo on 02/05/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCRGameControllerManager : NSObject
+
+@end
---
+++ gameController/JCRGameControllerManager.m
@@ -0,0 +1,13 @@
+//
+//  JCRGameControllerManager.m
+//  gameController
+//
+//  Created by César Manuel Pinto Castillo on 02/05/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import "JCRGameControllerManager.h"
+
+@implementation JCRGameControllerManager
+
+@end
