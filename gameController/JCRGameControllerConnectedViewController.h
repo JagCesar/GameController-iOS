b5731f15afa4e0f169ec225e4c8badc8d37d5c7b
César Pinto Castillo <cesar@jagcesar.se>
2014-05-02 23:10:29 -0700

    Deletes JCRViewController

--- gameController/JCRGameControllerConnectedViewController.h
+++ gameController/JCRGameControllerConnectedViewController.h
@@ -6,9 +6,7 @@
 //  Copyright (c) 2014 JagCesar. All rights reserved.
 //
 
-#import "JCRViewController.h"
-@class JCRGameController;
 
-@interface JCRGameControllerConnectedViewController : JCRViewController
+@interface JCRGameControllerConnectedViewController : UIViewController
 
 @end
